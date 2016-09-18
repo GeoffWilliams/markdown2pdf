1b6a87e146879d9f8b1c30b332ed81e0928d4845
Lynn Cyrin <firemagelynn@gmail.com>
2016-09-17 23:08:46 -0700

    upgrade theme finding (hopefully)

--- markdown2pdf/__init__.py
+++ markdown2pdf/__init__.py
@@ -13,14 +13,16 @@ def convert_md_2_pdf(filename, output=None, theme=None):
     if not output:
         output = '.'.join([filename.rsplit('.')[0], 'pdf'])
 
-    if theme is not None:
-        BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    if not theme is not None:
         css_file = theme
         if not os.path.exists(css_file):
-            css_file = os.path.join(BASE_DIR, 'themes/'+theme+'.css')
-
-        print(css_file)
+            css_file = os.path.join(
+                os.environ['VIRTUAL_ENV'], # <= might not exist
+                os.path.dirname(__file__),
+                'themes/'+theme+'.css',
+            )
         HTML(string=html).write_pdf(output, stylesheets=[css_file])
+
     else:
         HTML(string=html).write_pdf(output)
 
